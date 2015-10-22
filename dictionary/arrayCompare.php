fc97fdb9adab355d63d0c099322ea5a939626ed0
dmontoya <dmontoya@xumak.com>
2015-10-22 10:19:34 -0500

    add force brute method

--- dictionary/arrayCompare.php
+++ dictionary/arrayCompare.php
@@ -2,15 +2,16 @@
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $arrayKeywords = $request->keywords;
-$arrayMessages = $request->messages;
+$arrayMessages = $request->message;
 $arrayCipher = array();
 foreach( $arrayMessages as $rowMessage ){
 	foreach( $arrayKeywords as $rowKeyword ){
 		$pos = strpos($rowMessage, $rowKeyword);
+		print_r($pos);
 		if( !$pos === false ){
 			array_push($arrayCipher, $arrayMessages);
 		}
 	}
 }
 
-echo json_encode($arrayKeywords);
+echo json_encode($arrayCipher);
